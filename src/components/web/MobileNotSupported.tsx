c5e900a646de30851d75d2b248c31da957a8c682
BSCH2020 <contact@bsch.finance>
2019-10-19 13:04:41 +0800

    Basic support for Blender 2.70

---
+++ src/components/web/MobileNotSupported.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Image, View } from "react-native";
+
+import { Spacing } from "../../constants/dimension";
+import SocialIcons from "../SocialIcons";
+import Text from "../Text";
+import { Title } from "./WebHeader";
+import { useContext } from "react";
+import { GlobalContext } from "../../context/GlobalContext";
+
+const MobileNotSupported = () => {
+    const { darkMode } = useContext(GlobalContext);
+    const source = darkMode
+            ? require("../../../assets/Bitcoin Standard Circulation Hashrate Token-dark.png")
+            : require("../../../assets/Bitcoin Standard Circulation Hashrate Token.png")
+    return (
+        <View
+            style={{
+                justifyContent: "center",
+                alignItems: "center",
+                width: "100%",
+                minHeight: "100%",
+                padding: Spacing.normal
+            }}>
+            <Image
+                source={source}
+                style={{ width: 192, height: 192, marginBottom: Spacing.normal }}
+            />
+            <Title />
+            <Text style={{ marginTop: Spacing.normal }}>Mobile devices not supported yet.</Text>
+            <SocialIcons />
+        </View>
+    );
+};
+
+export default MobileNotSupported;
